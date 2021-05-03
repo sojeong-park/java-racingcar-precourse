42de99d74aa28092603ffbc1aeee82a30141413a
박소정 <psj6414@naver.com>
2021-05-03 12:19:07 +0900

    test : 자동차 전진/멈춤 판단 로직 테스트

--- src/test/java/car/PositionNumberTest.java
+++ src/test/java/car/PositionNumberTest.java
@@ -1,11 +1,22 @@
 package car;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import utils.RacingGameUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PositionNumberTest {
+    private PositionNumber positionNumber;
+    @BeforeEach
+    void setUp() {
+        positionNumber = new PositionNumber(1);
+    }
+
     @DisplayName("초기 위치값이 1 이하 경우 예외 확인")
     @ParameterizedTest
     @CsvSource({"-1", "0"})
@@ -15,5 +26,12 @@ class PositionNumberTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("초기 위치값은 1부터 시작입니다.");
 
+    }
+    @DisplayName("전진 멈춤 로직 체크")
+    @ParameterizedTest
+    @CsvSource({"4, 2", "0, 1", "9, 2"})
+    void checkIncreasePosition(int randomNumber, int resultPosition) {
+        positionNumber.checkIncreasePosition(randomNumber);
+        assertThat(positionNumber.getPosition()).isEqualTo(resultPosition);
     }
 }
